529ffd61ea0d69c387ccfc44798bcaf8c76417c7
amerca <amerca3@gmail.com>
2016-10-25 23:58:31 +0000

    bubble sort simple solution

--- bubble_sort/main.py
+++ bubble_sort/main.py
@@ -1,9 +1,17 @@
 """This is the entry point of the program."""
 
 
-def bubble_sort(list_of_numbers):
-    pass
+def bubble_sort(a):
+    for j in range(len(a)-1):
+        for i in range(len(a)-1):
+            if a[i+1]<a[i]:
+                a[i+1],a[i]=a[i],a[i+1]
+            print(a)
 
+
+
+        
+    return a
 
 if __name__ == '__main__':
     print(bubble_sort([9, 1, 3, 11, 7, 2, 42, 111]))
