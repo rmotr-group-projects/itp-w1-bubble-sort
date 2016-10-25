3ae76dd1153bb8ef8b22b02f97225556463dbec0
Batuhan Baserdem <batuhan.baserdem@stonybrook.edu>
2016-10-25 23:57:15 +0000

    modified:   bubble_sort/main.py

--- bubble_sort/main.py
+++ bubble_sort/main.py
@@ -2,7 +2,17 @@
 
 
 def bubble_sort(list_of_numbers):
-    pass
+    flag = True                 # Flag for passing arguments
+    while flag:                 # Iterate until a clean pass is achieved
+        flag = False            # If it is a clean pass, this will remaun so
+        # Run through all adjacent pairs
+        for i in range( len(list_of_numbers) - 1 ):
+            # Swap the adjacent pairs if they are not ordered
+            if list_of_numbers[i] > list_of_numbers[i+1]:
+                flag = True     # Raise flag as operated
+                [list_of_numbers[i], list_of_numbers[i+1]] = [list_of_numbers[i+1], list_of_numbers[i]]
+                
+    return list_of_numbers
 
 
 if __name__ == '__main__':
