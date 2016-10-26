e9114c022c1ab0e2d38b30decd9a4c22485c13a0
Adam B <adambaranczyk@yahoo.pl>
2016-10-26 03:19:37 +0000

    Whole done, 4 tests passed + python swap + nonopt variant(checking if swapped in iteration)

--- bubble_sort/main.py
+++ bubble_sort/main.py
@@ -7,7 +7,7 @@ def bubble_sort(list_of_numbers):
         flag = False
         for i, item in enumerate(list_of_numbers):
             if i+1<len(list_of_numbers) and list_of_numbers[i+1]<list_of_numbers[i] :
-                item, list_of_numbers[i+1]= list_of_numbers[i+1], item
+                list_of_numbers[i], list_of_numbers[i+1] = list_of_numbers[i+1], item
                 flag = True
                 
     return list_of_numbers    
@@ -19,14 +19,15 @@ def bubble_sort(list_of_numbers):
     while counter>0:
         for i, item in enumerate(list_of_numbers):
             if i+1<counter and list_of_numbers[i+1]<list_of_numbers[i] :
-                #item, list_of_numbers[i+1] = list_of_numbers[i+1], item
+                #list_of_numbers[i], list_of_numbers[i+1] = list_of_numbers[i+1], item
                 temp = list_of_numbers[i+1]
                 list_of_numbers[i+1] = item
                 list_of_numbers[i] = temp
-                print (item)
+                #print (item)
         counter -= 1        
                 
     return list_of_numbers    
     
 if __name__ == '__main__':
     print(bubble_sort([9, 1, 3, 11, 7, 2, 42, 111]))
+gfgfhdfghgfsdsdfsdfxcvxcv
