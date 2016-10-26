c5959fe3df3e6344a963e6a92a50e16341d3f055
Tracy Homer <thomer@mac.com>
2016-10-26 01:29:05 +0000

    Bubble Sort Solution

--- bubble_sort/main.py
+++ bubble_sort/main.py
@@ -2,7 +2,7 @@
 
 
 def bubble_sort(list_of_numbers):
-    for i in range(len(list_of_numbers)-1,1,-1):
+    for i in range(len(list_of_numbers)-1,0,-1):
         for j in range(i):
             if list_of_numbers[j] > list_of_numbers[j+1]:
                 placeholder = list_of_numbers[j]
