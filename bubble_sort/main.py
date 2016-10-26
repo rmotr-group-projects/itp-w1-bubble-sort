8bdc91e9f858eba099c612b36ec7dc1da9615ce6
Tracy Homer <thomer@mac.com>
2016-10-26 01:09:03 +0000

    Bubble Sort Project

--- bubble_sort/main.py
+++ bubble_sort/main.py
@@ -2,7 +2,15 @@
 
 
 def bubble_sort(list_of_numbers):
-    pass
+    for i in range(len(list_of_numbers)-1,1,-1):
+        for j in range(i):
+            if list_of_numbers[j] > list_of_numbers[j+1]:
+                placeholder = list_of_numbers[j]
+                list_of_numbers[j] = list_of_numbers[j+1]
+                list_of_numbers[j+1] = placeholder
+            
+    return list_of_numbers
+
 
 
 if __name__ == '__main__':
