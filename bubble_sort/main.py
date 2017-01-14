7db44bc92ec711134a80b16185ea1ef8c5ca9c7b
Michelangelo Prisciano <mike.prisciano@icloud.com>
2017-01-14 16:11:58 +0000

    First commit

--- bubble_sort/main.py
+++ bubble_sort/main.py
@@ -1,9 +1,17 @@
 """This is the entry point of the program."""
 
 
-def bubble_sort(list_of_numbers):
-    pass
+def bubble_sort(list_of_numbers) : 
+    current = 0
 
+    while current < len(list_of_numbers) : 
+        for i in range(len(list_of_numbers)) : 
+            if list_of_numbers[current] < list_of_numbers[i] : 
+                list_of_numbers[current], list_of_numbers[i] = list_of_numbers[i], list_of_numbers[current]
+            else : 
+                pass
+        current += 1
+    return list_of_numbers
 
 if __name__ == '__main__':
     print(bubble_sort([9, 1, 3, 11, 7, 2, 42, 111]))
