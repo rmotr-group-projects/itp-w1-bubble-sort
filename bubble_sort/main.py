da011b8e07446a98b25b2c99a4e07cda2a01b2f2
Michelangelo Prisciano <mike.prisciano@icloud.com>
2017-01-14 16:58:54 +0000

    Third commit

--- bubble_sort/main.py
+++ bubble_sort/main.py
@@ -4,7 +4,7 @@
 def bubble_sort(list_of_numbers) : 
     current = 0
 
-    if type(list_of_numbers) == list : 
+    if isinstance(list_of_numbers, list) : 
         while current < len(list_of_numbers) : 
             for i in range(len(list_of_numbers)) : 
                 if list_of_numbers[current] < list_of_numbers[i] : 
@@ -13,7 +13,7 @@ def bubble_sort(list_of_numbers) :
                     pass
             current += 1
         return list_of_numbers
-    else :
+    else : 
         return "Not valid list"
 
 if __name__ == '__main__':
