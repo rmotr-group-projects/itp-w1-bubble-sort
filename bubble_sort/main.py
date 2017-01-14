8fc3145139681df507e186352493995422982762
Ana Arsenovic <esova.anaarsenovic@gmail.com>
2017-01-14 19:00:56 +0000

    Innitial commit

--- bubble_sort/main.py
+++ bubble_sort/main.py
@@ -2,7 +2,11 @@
 
 
 def bubble_sort(list_of_numbers):
-    pass
+        for x in range(len(list_of_numbers) - 1, 0 , -1): # Iterate backwards through a list
+            for y in range(x): # Iterate again until all numbers are sorted
+                if list_of_numbers[y] > list_of_numbers[y + 1]:
+                    list_of_numbers[y], list_of_numbers[y + 1] = list_of_numbers[y + 1], list_of_numbers[y] # if number before is bigger, swap the values
+        return list_of_numbers     
 
 
 if __name__ == '__main__':
